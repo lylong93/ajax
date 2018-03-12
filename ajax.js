54823cd60bc240d24117f6264409a04a98ac99d1
Yoghurt <695498118@qq.com>
2018-03-12 15:11:56 +0800

    git

---
+++ ajax.js
@@ -0,0 +1,58 @@
+// ajax
+// jsonp
+(function() {
+    var Ajs = function() {
+        return new Ajs.prototype.init()
+    }
+    Ajs.prototype = {
+        init: function() {
+            return this
+        },
+        ajax: function(opt) {
+            var opt = opt || {}
+            if (!opt.url) return
+            url = opt.url
+            type = opt.type || 'get'
+            jsonp = opt.jsonp || 'callback'
+            timeout = opt.timeout || '3000'
+
+            if (window.XMLHttpRequest) {
+                var xml = new XMLHttpRequest()
+            } else {
+                var xml = new ActiveXObject('Microsoft.XMLHTTP')
+            }
+
+            switch (type.toLowerCase()) {
+                case 'get':
+                    xml.open('get', url, true)
+                    xml.send()
+                    break
+                case 'post':
+                    xml.open('post', url, true)
+                    xml.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+                    xml.send()
+                    break
+                case 'jsonp':
+                    var script = document.createElement('script')
+                    script.setAttribute('type', 'text/javascript')
+                    script.setAttribute('src', url)
+                    document.querySelector('head').appendChild(script)
+            }
+            xml.onreadystechange = function() {
+                if (xml.readyState === 4) {
+                    if (xml.status === 200) {
+                        console.log('ok')
+                    } else {
+                        console.log('err')
+                    }
+                }
+            }
+        },
+        jsonp: function() {
+
+        }
+    }
+    Ajs.prototype.init.prototype = Ajs.prototype;
+
+    window.Ajs = Ajs
+})()
